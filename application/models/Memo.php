59a9703ac378c1189a62d852c0717811352526fb
Daniel Meyer <daniel.meyer@gforces.pl>
2016-02-06 18:04:05 +0100

    Lowercase questions

--- application/models/Memo.php
+++ application/models/Memo.php
@@ -2,15 +2,13 @@
 
 class Model_Memo extends Zsamer_Db_Table_Row_Orm
 {
-    public $question;
-    public $answer;
 
     public function setQuestion($value) {
-        $this->question = $this->trim($value);
+        $this->setData('question', $this->trim($value));
     }
 
     public function setAnswer($value) {
-        $this->answer = $this->trim($value);
+        $this->setData('answer', $this->trim($value));
     }
 
     protected function trim($value) {
